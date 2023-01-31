e82459d53eb401a6c419f8bb273158811d8066cc
tr617 <tr617739@gmail.com>
2023-02-01 03:12:47 +0600

    update category

--- controller/category.js
+++ controller/category.js
@@ -44,3 +44,41 @@ exports.getCategory = async (req, res, next) => {
         next(err)
     }
 }
+
+exports.updateCategories = async (req, res, next) => {
+
+    try {
+        const {_id, name, parentId, type} = req.body;
+        // console.log(_id, name, parentId, type)
+
+        const updatedCategories = [];
+        if (name instanceof Array) {
+            for (let i = 0; i < name.length; i++) {
+                const category = {
+                    name: name[i],
+                    type: type[i],
+                };
+                if (parentId[i] !== "") {
+                    category.parentId = parentId[i];
+                }
+                const updatedCategory = await Category.findOneAndUpdate({_id: _id[i]}, category, {new: true}
+                );
+
+                updatedCategories.push(updatedCategory);
+            }
+            return res.status(201).json({updateCategories: updatedCategories});
+        } else {
+            const category = {name, type};
+            if (parentId !== "") {
+                category.parentId = parentId;
+            }
+            const updatedCategory = await Category.findOneAndUpdate({_id}, category, {
+                new: true,
+            });
+            return res.status(201).json({updatedCategory});
+        }
+    } catch
+        (err) {
+        next(err)
+    }
+}
--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import {requireSignin, requireSuperAdmin} from "../middleware/verifyUser";
 import upload from "../middleware/uploadImage";
-import {addCategory, getCategory} from "../controller/category";
+import {addCategory, getCategory, updateCategories} from "../controller/category";
 
 const router = express.Router();
 
 router.get('/category/getCategory', getCategory)
 router.post('/category/create', upload.single('picture'), requireSignin, requireSuperAdmin, addCategory)
+router.post('/category/update', upload.single('picture'), requireSignin, requireSuperAdmin, updateCategories)
 
 module.exports = router;
